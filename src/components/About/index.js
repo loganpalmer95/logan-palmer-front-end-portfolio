1fc03d80074d5a57ee0f1f18f87e964d733a5bd9
Logan Palmer <loganpalmer95@icloud.com>
2022-05-11 13:45:49 -0400

    reposition and resize images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "50%"}} alt="cover" />
       <div className="my-2">
         <p>
           I am a recent graduate of University of Central Florida's Full-Stack Software developer bootcamp leveraging a background in Customer Service and Operations Management working towards my career goal of becoming a Software Developer. In my off time, I enjoy spending time with family, friends and playing golf.
